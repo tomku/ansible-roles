6bd15700caa5b3fce6562d3dcbe73a8ed327c2e1
Tom Kurth <tom@losthalo.org>
2014-01-25 16:36:42 -0600

    General fixes for when interactive user isn't defined.

--- lisp-dev/files/ql-install.sh
+++ lisp-dev/files/ql-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo '(quicklisp-quickstart:install :path "~/.quicklisp/")
-(ql:update-client)
-(ql:update-all-dists)
-(quit)' | sbcl --noinform --load /tmp/quicklisp.lisp
+if [ ! -d ~/.quicklisp ] ; then
+    echo '(quicklisp-quickstart:install :path "~/.quicklisp/")
+    (ql:update-client)
+    (ql:update-all-dists)
+    (quit)' | sbcl --noinform --load /tmp/quicklisp.lisp
+fi
