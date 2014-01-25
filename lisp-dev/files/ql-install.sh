b751712c6c3432736eaea876fad2c65880f125e9
Tom Kurth <tom@losthalo.org>
2014-01-25 10:43:43 -0600

    Fix wrong use of $HOME in Quicklisp install script.

--- lisp-dev/files/ql-install.sh
+++ lisp-dev/files/ql-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo '(quicklisp-quickstart:install :path "$HOME/.quicklisp/")
+echo '(quicklisp-quickstart:install :path "~/.quicklisp/")
 (ql:update-client)
 (ql:update-all-dists)
 (quit)' | sbcl --noinform --load /tmp/quicklisp.lisp
