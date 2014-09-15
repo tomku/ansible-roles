172d7699036e4d941a7038eb204517a8edfad1ea
Tom Kurth <tom@losthalo.org>
2014-09-15 13:35:58 -0500

    Remove ansible-reload role.

--- ansible-reload/files/ansible-reload.sh
+++
@@ -1,7 +0,0 @@
-ansible-reload() {
-    ansible-playbook /vagrant/ansible/$1.yml -c local -i "127.0.0.1," $2 $3 $4 $5 $6
-}
-
-ansible-setup() {
-    ansible all -c local -i "127.0.0.1," -m setup
-}
